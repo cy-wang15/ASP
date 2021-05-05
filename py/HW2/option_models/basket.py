173d707eac7d4777064c45a39146b9380e295583
CongyuanWang <38725802+cy-wang15@users.noreply.github.com>
2021-05-05 16:54:55 +0800

    Update basket.py

--- py/HW2/option_models/basket.py
+++ py/HW2/option_models/basket.py
@@ -20,13 +20,13 @@ def basket_check_args(spot, vol, corr_m, weights):
     
 def basket_price_mc_cv(
     strike, spot, vol, weights, texp, cor_m, 
-    intr=0.0, divr=0.0, cp=1, n_samples=10000
+    intr=0.0, divr=0.0, cp_sign=1, n_samples=10000
 ):
     # price1 = MC based on BSM
     rand_st = np.random.get_state() # Store random state first
     price1 = basket_price_mc(
         strike, spot, vol, weights, texp, cor_m,
-        intr, divr, cp, True, n_samples)
+        intr, divr, cp_sign, True, n_samples)
     
     ''' 
     compute price2: mc price based on normal model
@@ -36,17 +36,22 @@ def basket_price_mc_cv(
     np.random.set_state(rand_st)  
     price2 = basket_price_mc(
         strike, spot, spot*vol, weights, texp, cor_m,
-        intr, divr, cp, False, n_samples)
+        intr, divr, cp_sign, False, n_samples)
     '''
-    price2 = 0
+    np.random.set_state(rand_st)  
+    price2 = basket_price_mc(
+        strike, spot, spot*vol, weights, texp, cor_m,
+        intr, divr, cp_sign, False, n_samples)
 
     ''' 
     compute price3: analytic price based on normal model
     
     price3 = basket_price_norm_analytic(
-        strike, spot, vol, weights, texp, cor_m, intr, divr, cp)
+        strike, spot, vol, weights, texp, cor_m, intr, divr, cp_sign)
     '''
-    price3 = 0
+    price3 = basket_price_norm_analytic(
+        strike, spot, vol, weights, texp, cor_m, intr, divr, cp_sign)
+    
     
     # return two prices: without and with CV
     return [price1, price1 - (price2 - price3)] 
@@ -54,7 +59,7 @@ def basket_price_mc_cv(
 
 def basket_price_mc(
     strike, spot, vol, weights, texp, cor_m,
-    intr=0.0, divr=0.0, cp=1, bsm=True, n_samples = 10000
+    intr=0.0, divr=0.0, cp_sign=1, bsm=True, n_samples = 10000
 ):
     basket_check_args(spot, vol, cor_m, weights)
     
@@ -72,20 +77,21 @@ def basket_price_mc(
         '''
         PUT the simulation of the geometric brownian motion below
         '''
-        prices = np.zeros_like(znorm_m)
+        prices = forward[:,None]*np.exp(-0.5 * texp * vol[:,None] ** 2 + np.sqrt(texp) * chol_m @ znorm_m)
+        pass
     else:
         # bsm = False: normal model
         prices = forward[:,None] + np.sqrt(texp) * chol_m @ znorm_m
     
     price_weighted = weights @ prices
     
-    price = np.mean( np.fmax(cp*(price_weighted - strike), 0) )
+    price = np.mean( np.fmax(cp_sign*(price_weighted - strike), 0) )
     return disc_fac * price
 
 
 def basket_price_norm_analytic(
     strike, spot, vol, weights, 
-    texp, cor_m, intr=0.0, divr=0.0, cp=1
+    texp, cor_m, intr=0.0, divr=0.0, cp_sign=1
 ):
     
     '''
@@ -94,20 +100,27 @@ def basket_price_norm_analytic(
     1. compute the forward of the basket
     2. compute the normal volatility of basket
     3. plug in the forward and volatility to the normal price formula
-    normal_formula(strike, spot, vol, texp, intr=0.0, divr=0.0, cp=1)
+    normal_formula(strike, spot, vol, texp, intr=0.0, divr=0.0, cp_sign=1)
     it is already imorted
     
     PUT YOUR CODE BELOW
     '''
-    
-    return 0.0
+    basket_check_args(spot, vol, cor_m, weights)
+    div_fac = np.exp(-texp*divr)
+    disc_fac = np.exp(-texp*intr)
+    forward = spot / disc_fac * div_fac
+    f_basket = forward @ weights
+    cov_m = vol * cor_m * vol[:,None]
+    vol_basket = np.sqrt(weights @ cov_m @ weights)
+    price = normal_formula(strike, f_basket, vol_basket, texp)
+    return price
 
-def spread_price_kirk(strike, spot, vol, texp, corr, intr=0, divr=0, cp=1):
+def spread_price_kirk(strike, spot, vol, texp, corr, intr=0, divr=0, cp_sign=1):
     div_fac = np.exp(-texp*divr)
     disc_fac = np.exp(-texp*intr)
     forward = spot / disc_fac * div_fac
     vol2 = vol[1]*forward[1]/(forward[1]+strike)
     vol_r = np.sqrt(vol[0]**2 + vol2*(vol2 - 2*corr*vol[0]))
-    price = disc_fac * bsm_formula(forward[1]+strike, forward[0], vol_r, texp, cp=cp)
+    price = disc_fac * bsm_formula(forward[1]+strike, forward[0], vol_r, texp, cp_sign=cp_sign)
 
     return price
